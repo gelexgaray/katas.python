63c8a7790296f561f8697f094d5ca7478fdcc39e
Gorka Elexgaray <gelexgaray@yahoo.es>
2019-08-02 16:27:50 +0200

    FIX: Find the largest palindrome made from the product of two 3-digit numbers. It wasn't finding the largest result

--- project_euler/004_largest_palindrome_product.py
+++ project_euler/004_largest_palindrome_product.py
@@ -11,12 +11,20 @@ def is_palindrome_number(number: int):
 
 
 def find_largest_palindrome(range_min: int, range_max: int):
-    for number1 in range(range_max, range_min, -1):
-        for number2 in range(range_max, range_min, -1):
-            product = number1 * number2
-            if is_palindrome_number(product):
-                print("%s * %s = %s" % (number1, number2, product))
-                return
+    largest_result = 0
+    largest_op1 = 0
+    largest_op2 = 0
+    for current_op1 in range(range_max, range_min, -1):
+        for current_op2 in range(range_max, range_min, -1):
+            test_result = current_op1 * current_op2
+            if is_palindrome_number(test_result):
+                if test_result > largest_result:
+                    largest_result = test_result
+                    largest_op1 = current_op1
+                    largest_op2 = current_op2
+                else:
+                    break
+    print("%s * %s = %s" % (largest_op1, largest_op2, largest_result))
     return
 
 
